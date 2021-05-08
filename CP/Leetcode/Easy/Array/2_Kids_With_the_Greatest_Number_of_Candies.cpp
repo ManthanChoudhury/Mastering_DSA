b5bb66dfe4f24b471755e62b3931ca425ffe9217
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2021-05-08 14:51:52 +0530

    Update 2_Kids_With_the_Greatest_Number_of_Candies.cpp

--- CP/Leetcode/Easy/Array/2_Kids_With_the_Greatest_Number_of_Candies.cpp
+++ CP/Leetcode/Easy/Array/2_Kids_With_the_Greatest_Number_of_Candies.cpp
@@ -4,7 +4,9 @@ public:
         int check = 0;
         vector<bool> ans;
         for(auto a:candies)check=max(check,a);
+        //! pro way to find max in vector without sort love it
         for(auto a:candies){
+            //bruteforce 
             if(a+extraCandies >= check ){
                 ans.push_back(true);
             }
